8dfb5f013837302be0cead1da5d3d55d451be521
ami0120 <a.fujii0120@gmail.com>
2021-04-17 23:39:28 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' } do
+  validates :first_name
+  validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'Full-width characters' } do
+  validates :first_name
+  validates :last_name
+  end
   validates :birthday, presence: true
 
 end
