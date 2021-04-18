e8313bcf44d3751f6e9afee5823ec7c869df1f10
ami0120 <a.fujii0120@gmail.com>
2021-04-18 22:51:45 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
   validates :nickname, presence: true
-  validates :encrypted_password, format: {with: /^[a-zA-Z0-9]+$/, message: 'Include both letters and numbers'}
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' } do
   validates :first_name
   validates :last_name
