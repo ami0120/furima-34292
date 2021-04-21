5d7989a1ab91a416809f7517c24103964fff7b4e
ami0120 <a.fujii0120@gmail.com>
2021-04-21 21:47:04 +0900

    itemcontrollerの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:name,:text,:category_id,:condition_id,:delivery_charge_id,:prefecture_id,:days_to_ship_id,:price).merge(:user_id: current_user.id)
+  end
+
 end
