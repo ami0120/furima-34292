f9c3250db6822d32f6f4b0d2e738673673d08267
ami0120 <a.fujii0120@gmail.com>
2021-04-21 21:37:20 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :days_to_ship_id, presence: true
+  validates :price, presence: true
 end
