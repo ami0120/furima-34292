a026c8d892e29923a8e9272440dab7ddbd0f22b1
ami0120 <a.fujii0120@gmail.com>
2021-04-22 22:13:26 +0900

    バリデーションの追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,5 +10,6 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, presence: true
   validates :prefecture_id, presence: true
   validates :days_to_ship_id, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ ,message: 'Half-width number'}
+  validates :price ,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 ,message: 'Out of setting range' }
 end
