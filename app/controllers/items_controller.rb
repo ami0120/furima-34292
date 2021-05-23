b2fc281b5e13432b37c81cc31205986ce0444817
ami0120 <a.fujii0120@gmail.com>
2021-05-23 23:39:31 +0900

    修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_new_user_session, only: [:new,:create]
+  before_action :authenticate_user!, only: [:new,:create]
   
   def index
   end
@@ -21,12 +21,6 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
-  end
-
-  def move_to_new_user_session
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
   end
 
 end
