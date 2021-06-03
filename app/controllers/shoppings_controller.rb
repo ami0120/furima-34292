e09cfce9fe4b7b32d51b77d722865ee6ef2d6193
ami0120 <a.fujii0120@gmail.com>
2021-06-03 21:22:14 +0900

    indexアクションの追記

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -1,10 +1,19 @@
 class ShoppingsController < ApplicationController
 
   def index
-    @shopping_address = ShoppingAdress.new
+    @shopping_address = ShoppingAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @shopping_address = ShoppingAddress.new(shopping_params)
   end
+
+  private
+
+  def shopping_params
+    params.require(:shopping_address).permit(:postal_code,:prefecture_id,:municipality,:address,:building_name,:phone_number).merge(user_id: current_user.id,item_id:params[:item_id])
+  end
+
 
 end
