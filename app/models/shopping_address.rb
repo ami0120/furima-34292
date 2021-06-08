c8568ac1b66b566c159e904db4d3b969aa65ebc3
ami0120 <a.fujii0120@gmail.com>
2021-06-08 21:28:57 +0900

    複数テーブル保存

--- app/models/shopping_address.rb
+++ app/models/shopping_address.rb
@@ -1,6 +1,6 @@
 class ShoppingAddress
   include ActiveModel::Model
-  attr_accessor :postal_code,:prefecture_id,:municipality,:address,:building_name,:phone_number,:user_id,:item_id
+  attr_accessor :postal_code,:prefecture_id,:municipality,:address,:building_name,:phone_number,:user_id,:item_id,:token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/,message: "is invalid. Enter it as follows (e.g. 123-4567)"}
@@ -12,7 +12,7 @@ class ShoppingAddress
   end
 
   def save
-    Shopping.create(user_id: user_id,item_id: item_id)
+    shopping = Shopping.create(user_id: user_id,item_id: item_id)
     Address.create(postal_code: postal_code,prefecture_id: prefecture_id,municipality: municipality,address: address,building_name: building_name,phone_number: phone_number,shopping_id: shopping.id)
   end
   
