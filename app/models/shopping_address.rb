ea27913b89ef61e96095c90393808cc207eca1ca
ami0120 <a.fujii0120@gmail.com>
2021-06-11 23:34:02 +0900

    バリデーションの修正

--- app/models/shopping_address.rb
+++ app/models/shopping_address.rb
@@ -5,8 +5,9 @@ class ShoppingAddress
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/,message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :prefecture_id, numericality:{ other_than: 1 }
+    validates :municipality
     validates :address
-    validates :phone_number
+    validates :phone_number,numericality: {only_integer: true}, length: { is: 11 }
     validates :user_id
     validates :item_id
   end
